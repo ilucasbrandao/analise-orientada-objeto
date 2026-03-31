fd0bc19c6713d5b4d3da8f54dd4723571ed3e985
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-03-31 05:42:44 -0300

    new: novos conceitos de estrutura relacional estudadas.

---
+++ Uniasselvi/src/relacional/Medico.java
@@ -0,0 +1,71 @@
+package relacional;
+
+public class Medico extends Funcionario{
+    private String especialidade;
+
+    public Medico(String nome, int idade, String cargo, String especialidade){
+        super(nome, idade, cargo);
+        this.especialidade = especialidade;
+    }
+
+    // Getters e Setters
+    public String getNome(){
+        return super.getNome();
+    }
+
+    public void setNome(String nome){
+        super.setNome(nome);
+    }
+
+    public void realizarProcedimento(){
+        System.out.println("Realizando uma ação clínica de um médico...");
+    }
+}
+
+// Subclasee Anestesista
+class Anestesista extends Medico{
+    public Anestesista(String nome, int idade, String cargo, String especialidade){
+        super(nome, idade, cargo, especialidade);
+    }
+    @Override
+    public void realizarProcedimento(){
+        System.out.println("Aplicando uma injeção anestésica...");
+    }
+}
+
+// Subclasee Obstetra
+class Obstetra extends Medico{
+    public Obstetra(String nome, int idade, String cargo, String especialidade){
+        super(nome, idade, cargo, especialidade);
+    }
+
+    @Override
+    public void realizarProcedimento(){
+        System.out.println("Preparando e retirando o bebê durante o parto...");
+    }
+}
+
+// Subclasse Pediatra
+class Pediatra extends Medico {
+    public Pediatra(String nome, int idade, String cargo, String especialidade) {
+        super(nome, idade, cargo, especialidade);
+    }
+
+    @Override
+    public void realizarProcedimento() {
+        System.out.println("Verificando a saúde do recém-nascido...");
+    }
+}
+
+
+// Subclasse Residente
+class Residente extends Medico {
+    public Residente(String nome, int idade, String cargo, String especialidade) {
+        super(nome, idade, cargo, especialidade);
+    }
+
+    @Override
+    public void realizarProcedimento() {
+        System.out.println("Em Treinamento para realizar procedimentos...");
+    }
+}
