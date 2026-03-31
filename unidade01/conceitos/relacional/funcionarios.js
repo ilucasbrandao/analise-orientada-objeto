73b00f2494fdd4683912e2c8c90bcd82a9030301
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-03-31 05:19:19 -0300

    delete: delete nos arquivos em javascript

--- unidade01/conceitos/relacional/funcionarios.js
+++
@@ -1,9 +0,0 @@
-import { Pessoa } from "./pessoa.js";
-
-export class Funcionario extends Pessoa {
-  #cargo;
-  constructor(nome, idade, cargo) {
-    super(nome, idade);
-    this.#cargo = cargo;
-  }
-}
